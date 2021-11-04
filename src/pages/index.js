024482fb3d64744562c57ac8d0ef9528620877df
loophalen <abharris@outlook.com>
2021-11-04 15:38:43 -0400

    Finished Gatsyb Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import Layout from "../components/layout";
 
@@ -5,6 +6,9 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial</p>
+      <StaticImage alt="Drew Beskin" src="../images/drewbeskin.png" />
+      <StaticImage alt="Nick Mallis" src="../images/nickmallis.png" />
+      <StaticImage alt="Grand Vapids" src="../images/grandvapids.png" />
     </Layout>
   );
 };
